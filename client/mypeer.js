6a880ecc70414d9be972587297b6ec1fabd2714b
Dan Coates <dcoates72@gmail.com>
2020-08-18 11:14:13 -0500

    Mess with connection stuff

--- client/mypeer.js
+++ client/mypeer.js
@@ -48,15 +48,15 @@
                     // Create own peer object with connection to shared PeerJS server
                     this.peer = new Peer(this.id_requested, { //'uh2', {
                         host: '185.146.28.139',
-                        //host: 'server.coateslab.org',
+                        host: 'server.coateslab.org',
                         port: '9000',
                         path: '/myapp',
                         debug: 3,
                         reliable: true,
 
                         // These are all attempts to get iPad/Macs working. Not sure if they work. 01-Aug-20
-                        sdpSemantics: 'plan-b',
-                        iceTransportPolicy: 'relay',
+                        //sdpSemantics: 'plan-b',
+                        //iceTransportPolicy: 'relay',
                     });
                     var peer=this.peer;
                     peer.on('open', function (id) {
