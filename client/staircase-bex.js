490f700e1120b69fb26d9bc16f39c3afae2a4903
Dan Coates <dcoates72@gmail.com>
2021-01-03 22:31:17 -0600

    Harrison/Bex letter expt

---
+++ client/staircase-bex.js
@@ -0,0 +1,340 @@
+'use strict';
+
+    // Harrison and Bex 2014
+    var flankers=['A','B','D','E','F','H','I','J','L','K','M','N','P','R','S','T','V'];
+
+    function generate_ori(nafc) {
+        var ori;
+        if (nafc==4) {
+            ori=getRandomInt(4)*90;
+        } else if (nafc==2) {
+            ori=getRandomInt(2)*180; // (0 or 1) * 180 : L or R
+        }
+        return ori;
+    }
+
+    // Base class:
+    // - curr_trial (woolean, size, etc.)
+    // - trial_history   
+
+    class staircase {
+            constructor(size,stair_N) {
+                this.stair_N=stair_N; // N-up-1down
+                this.restart(size); 
+                this.plot_log=false;
+            }
+
+            restart(size,nafc) {
+                this.stair_trial=0;
+                this.consecutive_corrects=0;
+                this.stair_size=size; // TODO
+                this.nReversals=0;
+                this.prev_corr=true; // assume first one correcta (still going "down")
+                this.trial_history=[];
+                this.nafc=nafc;
+            }
+
+            next() {
+                get_metap(); // TODO: we shouldn't access any UI stuff in here
+                do_trial();  // TODO: refactor out of UI
+
+                this.stair_trial += 1;
+                this.compute_mean();
+
+                var val=this.mean_cm/parseFloat(get_value('background'));
+                set_html("lblStair",`Staircase ${this.stair_trial} ${this.nReversals} threshold=${val.toPrecision(4)}`);
+
+                // Did enough reversals? 
+                if (this.nReversals>metap.staircase_reversals.length ) { // TODO: rmv from UI
+                    set_checked( "chkStair", false);
+                }
+            }
+
+            compute_mean() {
+                var revs_last=this.trial_history
+                    .filter(t1=>t1.is_reversal)         // extract reversals
+                    .filter( (word,idx,arr) => idx>1)   // remove first two
+
+                if (revs_last.length>0) {
+                    this.mean_last=revs_last.reduce( (total,t1)=>t1.size+total,0)/revs_last.length
+                } else {
+                    this.mean_last=0;
+                }
+
+                if (this.plot_log==true) { // Hack: means probably contrast, don't divide out cm
+                    this.mean_cm=this.mean_last;
+                } else {
+                    this.mean_cm=this.mean_last/200.0*get_value("box_size");
+                }
+            }
+
+            update(correct,ori_resp) {
+                var isReversal = false;
+
+                // N-up-1-down logic
+                if (true) {
+                    var step_size=metap.staircase_reversals[this.nReversals];
+                    var prev_size=this.stair_size;
+
+                    if (correct) {
+                        this.consecutive_corrects+=1;
+                        if (this.consecutive_corrects==(this.stair_N-1) ){
+                            this.stair_size /= step_size;
+                            this.consecutive_corrects=0;
+                            if (! (this.prev_corr)) {
+                                isReversal=true; // first reversal after an error
+                            } else {
+                                isReversal=false; // still going up
+                            }
+                            this.prev_corr=true;
+                        } else {
+                            // First correct of sequence, shouldn't yet change prev_corr, etc.
+                            ;
+                        }
+                    } else {
+                        this.stair_size *= step_size;
+                        this.consecutive_corrects=0;
+                        if (this.prev_corr) {
+                            isReversal=true; // first wrong after previous correct is considered reversal
+                        }
+                        this.prev_corr=false;
+                    }
+
+                    if (isReversal) {
+                        this.nReversals += 1;
+                    }
+
+                    // Log response -- TODO move out bad UI spaghetti , maybe log elsewhere, not just UI
+                    set_html("log",get_html("log")+"\n"+
+                        this.stair_trial+","+prev_size+","+trial_params['orientation']+','+ori_resp+','+correct+','+isReversal+','+this.nReversals);
+
+                    var trial1={'num':this.stair_trial, 'size':prev_size, 'ori': trial_params['orientation'],
+                        'resp': ori_resp, 'is_correct': correct, 'is_reversal': isReversal, 'num_reversals':
+                        this.nReversals, 'dir_reversal_down': this.prev_corr, 'x': this.stair_trial, 'y': prev_size};
+						// Make aliases for x and y just to make drawing easier
+
+                    this.trial_history.push(trial1);
+
+                    if (this.plot_log) {
+                        trial1.y = Math.log10(trial1.y)*25.0+100.0 
+                    };
+
+                    update_graph(trial1) //this.trial_history[this.trial_history.length-1]);
+					app1(this.trial_history); // TODO
+                    // Get next one:
+                    var oriNew=generate_ori(this.nafc) 
+                    // Set up trial parameters, which are merged with the code to do 1 trial
+                    set_value("trial",`trial_params={\n\torientation: ${oriNew},
+                        \n\tsize:${this.stair_size},
+                        \n\tcontrast:${this.contrast}
+                    }`);
+
+                    // Finished ?
+                    if (this.nReversals>=metap.staircase_reversals.length ) {
+                        set_checked( "chkStair", false); // TODO: out of UI
+                	var val=this.mean_cm/parseFloat(get_value('background'));
+                        set_html("lblStair",`FINISHED threshold=${val.toPrecision(4)}`);
+                        //beep(1,440,80);
+                    } else {
+                        this.next(); // execute next
+                    }
+                }
+            };
+        }
+
+    class MOCS {
+            constructor() {
+            }
+
+            restart(levels,repeats,nafc) {
+                this.levels=levels; 
+                this.repeats=repeats;
+                this.num_trial=0;
+                this.nafc=nafc;
+
+                var remaining=[]; // bc can't access 'this' in forEach (?)
+                this.levels.forEach(function(entry) { // List of valid trials yet to run
+                    var level1={}
+                    level1['which']=entry;
+                    level1['remaining']=repeats;
+                    remaining.push(level1);
+                });
+                this.remaining=remaining;
+                };
+
+            total_remaining() {
+                var total=0;
+                this.remaining.forEach(function(entry) { // List of valid trials yet to run
+                    total += entry['remaining'];
+                });
+                return total;
+            }
+
+            update(correct,ori_resp) {
+
+                set_html("log",get_html("log")+"\n"+
+                    this.num_trial+","+this.remaining[this.index_which]['which']+","+trial_params['orientation']+','+ori_resp+','+correct+',0,0,'+trial_params["flankers"]);
+
+                var spac=this.remaining[this.index_which]['which']
+                if (spac<10) {
+                    var yloc=spac*15; // CC spacings times ten
+                } else {
+                    var yloc=90;
+                }
+                var xloc=this.total_remaining()/2.0; // from right to left
+                var trial1={'is_correct': correct, 'x': xloc, 'y':yloc };
+                update_graph(trial1);
+
+                set_html("lblStair",`MOCS ${this.total_remaining()}`); //TOTAL
+
+                this.remaining[this.index_which]['remaining'] -= 1;
+                if (this.remaining[this.index_which]['remaining']==0) {
+                    this.remaining.splice(this.index_which,1); // remove 1 item from middle of array
+                }
+
+                // Finished ?
+                if (this.remaining.length < 1 ) {
+                    set_checked( "chkStair", false); // TODO: out of UI
+                    set_checked( "chkMOCS", false); // TODO: out of UI
+                    set_html("lblStair","FINISHED");
+                    //beep(1,440,80);
+                } else {
+                    this.next(); // execute next
+                }
+            };
+
+            next() {
+                this.index_which=getRandomInt(this.remaining.length);
+
+                // Get random orientation
+                var oriNew=generate_ori(this.nafc) 
+                
+                var flanker_code;
+                if (get_checked( "chkLetters" )) {
+                    shuffleArray(flankers);     // generate new flankers array also
+                    flanker_code=flankers[0]+flankers[1]+flankers[2]+flankers[3];
+                } else {
+                    var oris=['0','1','2','3'];
+                    shuffleArray(oris);
+                    flanker_code=oris.join('');
+                }
+
+                // Set up trial parameters, which are merged with the code to do 1 trial
+                set_value("trial",`trial_params={\n\torientation: ${oriNew},\n\tsize:`+
+                        this.remaining[this.index_which]['which']+
+                    ",\n\tflankers:'\\'"+flanker_code+"\\''\n}");
+
+                do_trial(); // TODO: figure out better place for this
+            };
+        }; // MOCS class
+
+
+    var num_manual_trial=0;      // trial count
+    var prev_answered=false;     // was previous trial answered already?
+
+    function manual_trial(which_size,nafc,is_YN) {
+        // Get random orientation
+        var oriNew=generate_ori(nafc);
+        if (is_YN) {oriNew=0}; // for Y/N task, always right-pointed
+
+        // Set up trial parameters, which are merged with the code to do 1 trial
+        set_value("trial",`trial_params={\n\torientation: ${oriNew},\n\tsize: ${map_size[which_size]}\n}`);
+
+        do_trial(); // TODO: figure out better place for this
+
+        prev_manual_trial={'ori':oriNew, 'size': which_size};
+        prev_answered=false;
+        num_manual_trial += 1;
+    }
+
+    // TODO: This UI doesn't really belong here
+    function update_manual_table_corr(nwhich,correct) {
+        var thisid='count_'+nwhich;
+        var strCurrent=get_html(thisid);
+        var fields=strCurrent.split('/');
+        set_html(thisid,(parseInt(fields[0])+correct)+'/'+fields[1]);
+    }
+    function update_manual_table_outof(nwhich) {
+        var thisid='count_'+nwhich;
+        var strCurrent=get_html(thisid);
+        var fields=strCurrent.split('/');
+        set_html(thisid,fields[0]+'/'+(parseInt(fields[1])+1));
+    }
+
+    // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+    function export_manual_table() {
+        var s = "";
+        s += "size,num_correct,num_presented";
+        s += ","+get_value("text_condition") + "\n";
+        for (var nrow = nrows_mocs_table; nrow>=0; nrow--) {
+            var thisid='count_'+nrow;
+            strCurrent=get_html(thisid);
+            fields=strCurrent.split('/');
+            // Only output cells for which one has been presented
+            if (parseInt(fields[1])>0) {
+                s+=nrow+","+fields[0]+","+fields[1]+"\n"
+            }
+        }
+        log.info(s)
+        set_html("log",s)
+
+        var owner=document.getElementById("table_text") 
+        owner.style.display="";
+        owner.hidden=false;
+
+        var el=document.getElementById("log") //.select();
+        var success= iosCopyToClipboard(el);
+        //success=navigator.clipboard.writeText(s);
+
+        owner.style.display="none";
+        owner.hidden=true;
+    }
+
+    function iosCopyToClipboard(el) {
+        var range = document.createRange();
+
+        el.contentEditable = true;
+        el.readOnly = false;
+        el.focus()
+        el.select()
+        range.selectNodeContents(el);
+
+        var s = window.getSelection();
+        s.removeAllRanges();
+        s.addRange(range);
+
+        el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+        document.execCommand('copy');
+    }
+
+    function process_manual(ori_resp, is_YN,allow_multiple) {
+        if ((!allow_multiple) && prev_answered) {
+            return;
+        } // noop if they already responded to this trial.
+        
+        prev_answered=true; // ...now they have!
+
+        var correct=(ori_resp==prev_manual_trial['ori']);
+        var nwhich=prev_manual_trial['size'];
+        if (is_YN) {
+            if (nwhich==0 && ori_resp==180) { // for Y/N trials, left&absent is correct,
+                correct=true;
+            } else {
+                if(nwhich!=0 && ori_resp==0) { // right&present is correct
+                    correct=true;
+                } else {
+                    correct=false;
+                };
+            }
+        };
+        log.info(correct);
+        update_manual_table_corr(nwhich,correct);
+
+        // Rescale our linear indexed scale (0-15) to the actual size scale, although sizes won't correspond
+        // Button one is a phantom button to leave space
+        var ylocGraph=(nwhich)*98/nrows_mocs_table;
+        var trial1={'is_correct': correct, 'x': num_manual_trial-1, 'y':ylocGraph };
+        update_graph(trial1);
+        //app1(this.trial_history); // TODO
+    }
---
+++ client/stims-bex.js
@@ -0,0 +1,301 @@
+'use strict';
+                var scale=0.1;
+                const GAMMA=2.4;
+                
+                function clear(colr) {
+                    ctx.fillStyle = colr;
+                    ctx.fillRect(0, 0, wid, height);
+                }
+
+                var val; //global for debuggin
+                // https://stackoverflow.com/questions/10521978/html5-canvas-image-contrast/34203206#34203206
+                function contrastImage(imgData, contrast){  //input range 0..1
+                        var d = imgData.data;
+                        for(var i=0;i<d.length;i+=4){   //r,g,b,a
+                            val=d[i];
+                            if (val<255)  {   
+                                val=(255.0-val)/255.0;           // convert pix from 0..255 to 1..0 (tested: pix=0.0 or 1.0)
+                                val=val*contrast;               // apply contrast
+                                val=1.0-val;                    // we are using negative contrast
+                                val=Math.floor( (val*255.0)+Math.random()-0.5); // apply noisy bit
+                                val=255.0*Math.pow(val/255.0, GAMMA); // approx. gamma correct
+                                val=Math.max(0,Math.min(val,255));  // clamp to 0..255
+                                val=parseInt(val);              
+                            };
+                            d[i] = val;
+                            d[i+1] = val;
+                            d[i+2] = val;
+                        }
+                        return imgData;
+                }
+
+                const image = new Image();
+                image.src = 'img/sloan-u.png'
+                const image_n = new Image();
+                image_n.src = 'img/sloan-n.png'
+                const image_v = new Image();
+                image_v.src = 'img/vernier.png'
+                const image_spot = new Image();
+                image_spot.src = 'img/spot.png'
+
+				const TO_RADIANS = Math.PI/180; 
+
+                // Assume 100% contrast. Stub for compatibility
+                function draw_e(posx,posy,siz,rotation) {
+                    draw_e_contrast(posx,posy,siz,rotation,1.0);
+                }
+
+                function draw_flanker(posx,posy,str,siz,contrast) {
+                    if (parseInt(str)>=0) {
+                        // It's a rotated U. 0=upright/normal
+                        draw_i_contrast(image,posx,posy,siz,parseInt(str)*90,contrast);
+                    } else {
+                        // Letter flanker
+                        draw_string(posx,posy+siz/2.0,str,siz);
+                    }
+                }
+
+                function draw_string(posx,posy,str,siz) {
+                    ctx.fillStyle="#000000";
+                    var newFont=siz+'px Sloan';
+                    ctx.font=newFont;
+                    ctx.textAlign="center";
+                    ctx.fillText(str,xc+posx,yc+posy);
+                }
+
+                function draw_e_contrast(posx,posy,siz,rotation,contrast) {
+                    ctx.imageSmoothingEnabled=false;
+					//ctx.save(); 
+					ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+					ctx.rotate(-rotation * TO_RADIANS); // this is clockwise. We want typical polar (90 is straight up)
+					ctx.drawImage(image, -siz/2, -siz/2, siz, siz);
+                    ctx.resetTransform();
+    				//ctx.restore(); 
+                    
+                    var imgData=ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+                    ctx.putImageData( contrastImage(imgData,contrast), 0, 0 );
+                }
+
+                // Pass in arbitrary image as "i"
+                function draw_i_contrast(i,posx,posy,siz,rotation,contrast) {
+                    ctx.imageSmoothingEnabled=true;
+					//ctx.save(); 
+					ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+					ctx.rotate(-rotation * TO_RADIANS); // this is clockwise. We want typical polar (90 is straight up)
+					ctx.drawImage(i, -siz/2, -siz/2, siz, siz);
+                    ctx.resetTransform();
+    				//ctx.restore(); 
+                    
+                    // Too slow -- Feb 2021 !
+                    //var imgData=ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+                    //ctx.putImageData( contrastImage(imgData,contrast), 0, 0 );
+                }
+
+                function draw_v_contrast(posx,posy,siz,rotation,contrast) {
+                    ctx.imageSmoothingEnabled=false;
+					//ctx.save(); 
+                    if (rotation==0) {
+					    ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+					    ctx.drawImage(image_v, -siz/2, -siz/2, siz, siz);
+                    } else {
+					    ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+                        ctx.translate(image_v.width,0); // Redundant??!
+                        ctx.scale(-1,1);
+					    ctx.drawImage(image_v, -siz/2, -siz/2, siz, siz);
+                    }
+                    ctx.resetTransform();
+    				//ctx.restore(); 
+                    
+                    var imgData=ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+                    ctx.putImageData( contrastImage(imgData,contrast), 0, 0 );
+                }
+
+                function draw_spot_contrast(posx,posy,siz,rotation,contrast) {
+                    ctx.imageSmoothingEnabled=false;
+					//ctx.save(); 
+                    ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+
+                    if (rotation==0) {
+					    ctx.setTransform(1.0, 0, 0, 1.0, posx+xc, posy+yc); // sets scale and origin
+					    ctx.drawImage(image_spot, -siz/2, -siz/2, siz, siz);
+					    //ctx.drawImage(image_v, -siz/2, -siz/2, siz, siz);
+                    } else {
+                        ; //noop: don't draw
+                    }
+
+                    ctx.resetTransform();
+    				//ctx.restore(); 
+                    
+                    var imgData=ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+                    ctx.putImageData( contrastImage(imgData,contrast), 0, 0 );
+                }
+
+                // Assume 100% contrast. Stub for compatibility
+                function draw_letter(which,ori,posx,posy,siz,color,barsep,esep,flankers) {
+                    draw_letter2(which,ori,posx,posy,siz,color,barsep,esep,1.0,flankers);
+                }
+
+                // This one takes the contrast
+                function draw_letter2(which,ori,posx,posy,siz,color,barsep,esep,contrast,flankers) {
+                    if (siz==0) {return;} // make sure noop for size 0, and no weirdness
+
+                    if (which=='E') {
+							draw_e_contrast(posx,posy,siz*5.0,ori,contrast);
+                    } else if (which=='|') {
+                            // Image for vernier is bigger to allow lines to be longer
+							draw_v_contrast(posx,posy,siz*20.0,ori,contrast);
+                    } else if (which=='.') {
+                            // Image for vernier is bigger to allow lines to be longer
+							draw_spot_contrast(posx,posy,siz*1.0,ori,contrast);
+                    }
+
+                    if (esep>=0) {
+							draw_flanker(posx +siz*5*esep,posy,flankers[0],siz*5,contrast);
+							draw_flanker(posx,-siz*5*esep+posy,flankers[1],siz*5,contrast);
+							draw_flanker(posx -siz*5*esep,posy,flankers[2],siz*5,contrast);
+							draw_flanker(posx,+siz*5*esep+posy,flankers[3],siz*5,contrast);
+                    }
+                    if (barsep>=0) { // TODO: Untested; not centered well?
+                        ctx.beginPath();
+                        ctx.lineWidth=siz;
+                        ctx.strokeStyle=color;
+                        ctx.moveTo(xc-siz*barsep+posx, yc-siz*barsep/2.0+posy);
+                        ctx.lineTo(xc-siz*barsep+posx, yc+siz*barsep/2.0+posy);
+
+                        ctx.moveTo(xc+siz*barsep+posx, yc-siz*barsep/2.0+posy);
+                        ctx.lineTo(xc+siz*barsep+posx, yc+siz*barsep/2.0+posy);
+
+                        ctx.moveTo(xc-siz*barsep/2.0+posx, yc-siz*barsep+posy);
+                        ctx.lineTo(xc+siz*barsep/2.0+posx, yc-siz*barsep+posy);
+
+                        ctx.moveTo(xc-siz*barsep/2.0+posx, yc+siz*barsep+siz+posy);
+                        ctx.lineTo(xc+siz*barsep/2.0+posx, yc+siz*barsep+siz+posy);
+                        ctx.stroke()
+                    }
+                }
+
+                function fixation(x,y,siz,wid,colr,style) {
+                    ctx.beginPath();
+                    ctx.lineWidth=wid;
+                    ctx.strokeStyle=colr;
+                    if (style!="+") {
+                        ctx.moveTo(x+xc+siz, y+yc+siz);
+                        ctx.lineTo(x+xc+siz/2.0, y+yc+siz/2.0);
+
+                        ctx.moveTo(x+xc-siz, y+yc+siz);
+                        ctx.lineTo(x+xc-siz/2.0, y+yc+siz/2.0);
+
+                        ctx.moveTo(x+xc-siz, y+yc-siz);
+                        ctx.lineTo(x+xc-siz/2.0, y+yc-siz/2.0);
+
+                        ctx.moveTo(x+xc+siz, y+yc-siz);
+                        ctx.lineTo(x+xc+siz/2.0, y+yc-siz/2.0);
+                    } else {
+                        ctx.moveTo(x+xc, y+yc-siz);
+                        ctx.lineTo(x+xc, y+yc+siz);
+                        ctx.moveTo(x+xc-siz, y+yc);
+                        ctx.lineTo(x+xc+siz, y+yc);
+                    }
+                    ctx.stroke()
+                }
+
+                function up(posx,posy,bar,colr) {
+                    ctx.beginPath();
+                    ctx.lineWidth=bar;
+                    ctx.strokeStyle=colr;
+                    ctx.moveTo(xc-bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc+bar*2.5+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc+bar*2.5+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc-bar*2+posy);
+                    ctx.moveTo(xc+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc+posx, yc+bar*2.5+posy);
+                    ctx.stroke()
+                };
+
+                function down(posx,posy,bar,colr) {
+                    ctx.beginPath();
+                    ctx.moveTo(xc+bar*2+posx, yc+bar*2.5+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc+bar*2.5+posy);
+                    ctx.moveTo(xc+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc+posx, yc+bar*2.5+posy);
+                    ctx.lineWidth=bar;
+                    ctx.strokeStyle=colr;
+                    ctx.stroke()
+                };
+
+                function left(posx,posy,bar,colr) {
+                    ctx.beginPath();
+                    ctx.moveTo(xc-bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc+bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc+bar*2+posy);
+                    ctx.moveTo(xc-bar*2+posx,yc+posy);
+                    ctx.lineTo(xc+bar*2+posx,yc+posy);
+
+                    ctx.lineWidth=bar;
+                    ctx.strokeStyle=colr;
+                    ctx.stroke()
+                };
+
+                function right(posx,posy,bar,colr) {
+                    ctx.beginPath();
+                    ctx.moveTo(xc+bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc-bar*2+posy);
+                    ctx.lineTo(xc-bar*2+posx, yc+bar*2+posy);
+                    ctx.lineTo(xc+bar*2+posx, yc+bar*2+posy);
+                    ctx.moveTo(xc+bar*2+posx,yc+posy);
+                    ctx.lineTo(xc-bar*2+posx,yc+posy);
+
+                    ctx.lineWidth=bar;
+                    ctx.strokeStyle=colr;
+                    ctx.stroke()
+                };
+
+        function square(sid,colr) {
+            var c = document.getElementById("canvas");
+            var ctx = c.getContext("2d");
+            var wid=canvas.width;
+            var height=canvas.height;
+
+            ctx.fillStyle = colr;
+
+            // Center it
+            ctx.fillRect(wid/2-sid/2, height/2-sid/2, sid, sid);
+            };
+
+
+var canvVis=document.getElementById("canvas2");
+var canvTemp=document.createElement('canvas','canvas_render')
+function resizeTo(canvas,pct){
+	/* https://stackoverflow.com/questions/34866171/resize-html-canvas-with-scale */
+	var ctx_vis=canvVis.getContext("2d");
+
+	// Get from main "canvas" which is visible, but just for rendering by main program
+	// grab out of this, into canvTemp. Then draw this on the (visible) canvVis
+	var cw=canvas.width;
+	var ch=canvas.height;
+	canvTemp.width=cw;
+	canvTemp.height=ch;
+	var ctx_temp=canvTemp.getContext("2d");
+
+	var frac_w=canvVis.width/canvas.width
+	var frac_h=canvVis.height/canvas.height 
+	var shrink=1;
+	if (frac_w<frac_h) {
+		shrink=frac_w;
+	} else {
+		shrink=frac_h;
+	}
+	ctx_temp.drawImage(canvas,0,0);
+	//canvas.width*=pct;
+	//canvas.height*=pct;
+	//ctx.clearRect(0,0,cw,ch);
+
+	// center image of pair on display
+	var x_center=canvVis.width/2-canvas.width*shrink/2 
+	var y_center=canvVis.height/2-canvas.height*shrink/2 
+	ctx_vis.drawImage(canvTemp,x_center,y_center,cw*shrink,ch*shrink);
+	}
+
